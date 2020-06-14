eb8adecdcf8c2389ebffd4981f7d4dfbf164aad3
De-Anna <61568952+dclarke044@users.noreply.github.com>
2020-06-14 16:23:40 -0400

    final commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,40 +4,61 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Console.log the UFO data from data.js
 console.log(data);
 
-data.forEach((weatherReport) => {
+data.forEach((UFOvisits) => {
     var row = tbody.append("tr");
-    Object.entries(weatherReport).forEach(([key, value]) => {
+    Object.entries(UFOvisits).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
 
-  // Select the form
-var form = d3.select("#form");
+  // Complete function to clear existing table before printing filtered Data
+function removeTableBody(){
+  $('#ufo-table tbody').empty();
+}
+
+  // define input field
+var inputField = d3.select("form-control");
+
+  // Select the button
+var button = d3.select("#filter-btn");
 
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+inputField.on("submit",runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#patient-form-input");
+  var inputElement = d3.select("#datetime");
 
       // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
   console.log(inputValue);
-  console.log(sightings);
+  console.log(tableData);
 
-  var filteredData = sightings.filter(sightings => sightings.date/time === inputValue);
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 
   console.log(filteredData);
+
+  var tbody = d3.select("tbody");
+
+  // clear table
+  removeTableBody();
+
+  // Console.log the filtered data from data.js
+  console.log(filteredData);
+  
+  filteredData.forEach((filteredUFOvisits) => {
+      var row = tbody.append("tr");
+      Object.entries(filteredUFOvisits).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
 };
